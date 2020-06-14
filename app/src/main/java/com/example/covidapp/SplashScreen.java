6fbbfe14778aa54dcdeb1eef2331b798f8e69226
seongjinah <5829341@naver.com>
2020-06-14 23:43:48 +0900

    Finish activity_splash_screen.xml

--- app/src/main/java/com/example/covidapp/SplashScreen.java
+++ app/src/main/java/com/example/covidapp/SplashScreen.java
@@ -2,13 +2,27 @@ package com.example.covidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class SplashScreen extends AppCompatActivity {
 
+    ImageButton tonextpagebtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        tonextpagebtn = (ImageButton)findViewById(R.id.toNextPageBtn);
+        tonextpagebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
